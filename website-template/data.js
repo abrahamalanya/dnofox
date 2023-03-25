6146fe6c44b9f8142e7eaad8c36ab919976d626e
Abraham Alanya <alanyaoficial@gmail.com>
2023-03-24 23:33:47 -0500

    index completado

---
+++ website-template/data.js
@@ -0,0 +1,25 @@
+const testimonials = [
+    {
+        name: 'Juan Pérez',
+        text: 'El mejor restaurante de la ciudad, sin duda alguna.',
+        image: 'img/user_1.jpg'
+    },
+    {
+        name: 'María García',
+        text: 'La comida es deliciosa y el servicio es excelente.',
+        image: 'img/user_2.jpg'
+    },
+    {
+        name: 'Zoe Joe',
+        text: '"El restaurante es increíble, la comida es deliciosa y el ambiente es acogedor. ¡Definitivamente volveré!"',
+        image: 'img/user_3.jpg'
+    }
+];
+
+const menus = [
+    {
+        name: 'Hamburguesa',
+        price: 'S/10',
+        image: 'img/menus/piqueo_1.jpg'
+    }
+]
---
+++ website-template/script.js
@@ -0,0 +1,30 @@
+
+function renderTestimonial(index) {
+    const container = document.querySelector('.testimonials-container');
+    const testimonial = testimonials[index];
+    const html = `
+        <div class="testimonial">
+            <img src="${testimonial.image}" alt="${testimonial.name}">
+            <blockquote>
+                <p>${testimonial.text}</p>
+                <cite>${testimonial.name}</cite>
+            </blockquote>
+        </div>
+                `;
+    container.innerHTML = html;
+}
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+let currentIndex = 0;
+
+prevBtn.addEventListener('click', () => {
+    currentIndex = currentIndex > 0 ? currentIndex - 1 : testimonials.length - 1;
+    renderTestimonial(currentIndex);
+});
+
+nextBtn.addEventListener('click', () => {
+    currentIndex = currentIndex < testimonials.length - 1 ? currentIndex + 1 : 0;
+    renderTestimonial(currentIndex);
+});
+renderTestimonial(0);
